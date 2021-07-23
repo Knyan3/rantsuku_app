3252e9f5c37ed81124e21007fcb8af451583dbab
Keishiro Suzuki <keishiro.basstennis@gmail.com>
2021-07-22 23:37:29 -0700

    third commit

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -5,12 +5,15 @@ import 'package:rantsuku_app/ranking.dart';
 class MainModel extends ChangeNotifier {
   List<Ranking> rankings = [];
 
-  Future getRankings() async {
-    final snapshot =
-        await FirebaseFirestore.instance.collection('rankings').get();
-    final docs = snapshot.docs;
-    final rankings = docs.map((doc) => Ranking(doc)).toList();
-    this.rankings = rankings;
-    notifyListeners();
+  void getRankings() {
+    final snapshots =
+        FirebaseFirestore.instance.collection('rankings').snapshots();
+    snapshots.listen((snapshot) {
+      final docs = snapshot.docs;
+      final rankings = docs.map((doc) => Ranking(doc)).toList();
+      rankings.sort((a, b) => b.createdAt.compareTo(a.createdAt));
+      this.rankings = rankings;
+      notifyListeners();
+    });
   }
 }
